75b7dab70f5105ea657b2d882db0e3e1e0da2050
cyrsgl <urbanocyris4@gmail.com>
2026-03-30 12:16:25 +0800

    Initial commit: simple C# grade app

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? name;
+        double grade;
+
+        Console.Write("Enter student name: ");
+        name = Console.ReadLine();
+
+        Console.Write("Enter grade: ");
+        grade = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\n--- Result ---");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Grade: " + grade);
+
+        if (grade >= 75)
+        {
+            Console.WriteLine("Remarks: Passed");
+        }
+        else
+        {
+            Console.WriteLine("Remarks: Failed");
+        }
+    }
+}
